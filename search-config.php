12203457b960e50d9a8775262a0148b7c16c6e47
Kripasindhu <krips.from.iit.kgp@gmail.com>
2015-08-21 19:30:54 +0200

    updated documentation

--- search-config.php
+++ search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Mon Jun 29 2015 20:50:11 for Open Detection by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.6 ",
+  'logo' => "Generated on Fri Aug 21 2015 19:26:07 for Open Detection by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.6 ",
 );
 
 </script>
