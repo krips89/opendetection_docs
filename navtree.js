b84b23557b8287082b11ae144dc40316aa81484b
Kripasindhu <krips.from.iit.kgp@gmail.com>
2015-10-03 20:59:01 +0200

    updated

--- navtree.js
+++ navtree.js
@@ -1,31 +1,32 @@
 var NAVTREE =
 [
   [ "Open Detection", "index.html", [
-    [ "About", "index.html", null ],
-    [ "OD Manual", "tutorial_root.html", "tutorial_root" ],
-    [ "Namespaces", null, [
-      [ "Namespace List", "namespaces.html", "namespaces" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ],
-        [ "Enumerations", "namespacemembers_enum.html", null ],
-        [ "Enumerator", "namespacemembers_eval.html", null ]
-      ] ]
-    ] ],
-    [ "Classes", null, [
-      [ "Class List", "annotated.html", "annotated" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", "functions_func" ],
-        [ "Variables", "functions_vars.html", null ],
-        [ "Typedefs", "functions_type.html", null ],
-        [ "Enumerations", "functions_enum.html", null ],
-        [ "Enumerator", "functions_eval.html", null ]
-      ] ]
-    ] ],
-    [ "Examples", "examples.html", "examples" ]
+    [ "User Guide", "tutorial_root.html", "tutorial_root" ],
+    [ "API Documentation", "usergroup0.html", [
+      [ "Namespace List", null, [
+        [ "Namespace List", "namespaces.html", "namespaces" ],
+        [ "Namespace Members", "namespacemembers.html", [
+          [ "All", "namespacemembers.html", null ],
+          [ "Functions", "namespacemembers_func.html", null ],
+          [ "Enumerations", "namespacemembers_enum.html", null ],
+          [ "Enumerator", "namespacemembers_eval.html", null ]
+        ] ]
+      ] ],
+      [ "Class List", null, [
+        [ "Class List", "annotated.html", "annotated" ],
+        [ "Class Index", "classes.html", null ],
+        [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+        [ "Class Members", "functions.html", [
+          [ "All", "functions.html", "functions_dup" ],
+          [ "Functions", "functions_func.html", "functions_func" ],
+          [ "Variables", "functions_vars.html", null ],
+          [ "Typedefs", "functions_type.html", null ],
+          [ "Enumerations", "functions_enum.html", null ],
+          [ "Enumerator", "functions_eval.html", null ]
+        ] ]
+      ] ],
+      [ "Examples", "examples.html", "examples" ]
+    ] ]
   ] ]
 ];
 
@@ -33,7 +34,7 @@ var NAVTREEINDEX =
 [
 "annotated.html",
 "classod_1_1g2d_1_1_o_d_h_o_g_detector.html",
-"namespaces.html"
+"namespacesvmlight.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
